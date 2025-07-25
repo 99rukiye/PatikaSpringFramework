afa543127d99c3e3f9c5f04804735d9b916f7f96
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-07-25 20:55:34 +0300

    Java Tutorials

---
+++ src/main/java/com/rukiye/tutorials/JavaTutorials.java
@@ -0,0 +1,57 @@
+package com.rukiye.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+
+    public static void main(String[] args) {
+
+//        Arrays.sort();
+//        this.
+//        super
+//        break continue return
+//        cast
+//        primitive type
+//        wrapper class
+//        Random
+//        Math
+//        %s %d %f
+//        final
+//        static
+//        decoder encoder
+//        instanceof
+//        public private protected
+//        String
+//        StringBuilder
+//        StringTokenizer
+//        StringBuffer
+//        Enum
+//        Multithread
+//        SOLID
+//        MONAD
+//        List Set Map
+//        Generic
+//        Iterator
+//        OOP
+//        Interface
+//        Abstract
+//        Inheritance
+//        UML - + __
+//        POJO
+//        bean
+//        Serileştirme
+//        IO
+//        JAR WAR
+//       Dağıtık
+//       server
+//        exception error
+
+//        stream, sort, map, filter
+//        optional
+//        lambda expression
+//        method referances ::
+//        Design Pattern
+
+        
+    }
+}
